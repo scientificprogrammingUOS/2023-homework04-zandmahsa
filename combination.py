af93ba1b74fc334c0f04a7a33cd778b803161f89
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2023-05-16 21:16:26 +0330

    Update combination.py

--- combination.py
+++ combination.py
@@ -2,12 +2,19 @@ import numpy as np
 
 # implement your function to combine two numpy arrays 
 
-def combination():
-    # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+def combination(array_one, array_two, axiss=0):
+    array_one = np.squeeze(array_one)
+    array_two = np.squeeze(array_two)
+    a1 = array_one.ndim
+    a2 = array_two.ndim
+    if a1 == a2: 
+        return (np.concatenate((array_one,array_two),axis=axiss))
+    else:
+        return ("No")
 
 
 if __name__ == "__main__":
     # use this for your own testing!
-
+    #combination([1,2],[3,4],0)
     pass
+
