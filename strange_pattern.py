4a7d26e72102667f2ade641bf2a1795053fc1cbf
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2023-05-16 21:17:06 +0330

    Update strange_pattern.py

--- strange_pattern.py
+++ strange_pattern.py
@@ -6,8 +6,6 @@ def strange_pattern(hight, width):
     array = np.arange(width*hight).reshape(hight, width)
     mask =  array%3 == 0 
     return mask
-    
-
 
 if __name__ == "__main__":
     # use this for your own testing!
